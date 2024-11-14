ed714b797a25ea7e7b126f8a920e59e96800c835
parostatkiem <jan.sudczak@pubnub.com>
2024-11-14 10:15:17 +0100

    remove secret

--- src/pubnub.ts
+++ src/pubnub.ts
@@ -5,7 +5,6 @@ const pubnub = new PubNub({
   //   subscribeKey: 'demo',
   subscribeKey: 'sub-c-fe4d8295-1af3-4933-bb04-d95844afb003', // event generator
   //   subscribeKey: 'sub-c-d9270297-5a17-4084-9e13-5bf43938f5d4', // Markus' generator
-  //   secretKey: 'sec-c-ZDEyNjc0YTctNjFkYS00MGY1LWEwMzQtNDc0Y2JjZDZiMjk0',
   userId: 'traffic-visualizer', // Assuming userId is correctly typed as string
 });
 
