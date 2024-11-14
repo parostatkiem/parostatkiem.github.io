140e356b4472b078695ce0ac0706ed2582a97e2f
parostatkiem <jan.sudczak@pubnub.com>
2024-11-14 16:53:05 +0100

    use local generator

--- src/pubnub.ts
+++ src/pubnub.ts
@@ -3,8 +3,11 @@ import PubNub from 'pubnub';
 const pubnub = new PubNub({
   // publishKey: "myPublishKey",
   //   subscribeKey: 'demo',
-  subscribeKey: 'sub-c-fe4d8295-1af3-4933-bb04-d95844afb003', // event generator
+  // subscribeKey: 'sub-c-fe4d8295-1af3-4933-bb04-d95844afb003', // event generator
   //   subscribeKey: 'sub-c-d9270297-5a17-4084-9e13-5bf43938f5d4', // Markus' generator
+
+  //local generator
+  subscribeKey: 'sub-c-bd96b9b2-7bec-4067-a0f8-a233ff413d84',
   userId: 'traffic-visualizer', // Assuming userId is correctly typed as string
 });
 
